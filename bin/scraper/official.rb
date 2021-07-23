6959de086fae634458a668445c0404ace3e03325
Tony Bowden <tony@tmtm.com>
2021-07-23 15:25:03 +0300

    Initial scrapers and data

---
+++ bin/scraper/official.rb
@@ -0,0 +1,35 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'open-uri/cached'
+require 'pry'
+
+class MemberList
+  # details for an individual member
+  class Member < Scraped::HTML
+    field :name do
+      noko.css('.name').text.split(' ').reverse.join(' ').tidy
+    end
+
+    field :position do
+      noko.css('.employment').text.tidy
+    end
+  end
+
+  # The page listing all the members
+  class Members < Scraped::HTML
+    field :members do
+      member_container.map { |member| fragment(member => Member).to_h }
+    end
+
+    private
+
+    def member_container
+      noko.css('.team .team-item')
+    end
+  end
+end
+
+url = 'https://www.kmu.gov.ua/en/team'
+puts EveryPoliticianScraper::ScraperData.new(url).csv
