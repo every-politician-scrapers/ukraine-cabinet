661418e8076bf4491e209c00969acf71a06f0e6a
Tony Bowden <tony@tmtm.com>
2021-12-22 13:36:39 +0200

    update to latest wikibase-cli scripts

--- create-office.js
+++ create-office.js
@@ -1,24 +1,27 @@
 // wd create-entity create-office.js "Minister for X"
-module.exports = (label) => {
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = (label, ministry) => {
+  claims = {
+    P31:   { value: 'Q294414' }, // instance of: public office
+    P279:  { value: 'Q83307'  }, // subclas of: minister
+    P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
+    P1001: { value: meta.jurisdiction.id },
+    P361:  { value: meta.cabinet.parent }
+  }
+
+  if(ministry)   claims['P2389']  = ministry
+
   return {
     type: 'item',
     labels: {
       en: label,
     },
     descriptions: {
-      en: 'position in the Cabinet of Ukraine',
+      en: `cabinet position in ${meta.jurisdiction.name}`,
     },
-    claims: {
-      P31:   { value: 'Q294414' }, // instance of: public office
-      P279:  { value: 'Q83307'  }, // subclas of: minister
-      P17:   { value: 'Q212'    }, // country: Ukraine
-      P1001: { value: 'Q212'    }, // jurisdiction: Ukraine
-      P361: {
-        value: 'Q613729',          // part of: Council of Ministers of Ukraine
-        references: {
-          P854: 'https://www.kmu.gov.ua/en/team'
-        },
-      }
-    }
+    claims: claims
   }
 }
