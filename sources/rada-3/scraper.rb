8b78ecd707dc22f0f9bec42ecda704d6292ed09b
Tony Bowden <tony@tmtm.com>
2022-02-14 12:28:25 +0200

    compare uawiki members of the 3th VRada

---
+++ sources/rada-3/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'списку'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name party _ _ no start end].freeze
+    end
+
+    def startDate
+      start_cell.css('span').text.tidy
+    end
+
+    def endDate
+      end_cell.css('span').text.tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
