805c7530a78d4ec8cd2c00452b1b216c1cef6b3d
Tony Bowden <tony@tmtm.com>
2022-03-02 08:52:48 +0200

    compare list of Ambassadors to the US

---
+++ sources/ambassador-usa/scraper.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Image'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[ordinal _ _ name image dates].freeze
+    end
+
+    def raw_combo_date
+      super[/\((.*)\)/, 1]
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
