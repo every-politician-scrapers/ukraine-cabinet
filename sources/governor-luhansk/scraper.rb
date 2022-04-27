0bf1b0c9f50eb79cc08fab892744d1ee9458012a
Tony Bowden <tony@tmtm.com>
2022-04-27 07:52:08 +0300

    compare Luhansk Governors

---
+++ sources/governor-luhansk/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'головування'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no name dates].freeze
+    end
+
+    def raw_combo_date
+      super.sub(/^з /, '')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
