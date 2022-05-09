b21a1a4717ecb3d28eb5260f1b89acf000ad09ed
Tony Bowden <tony@tmtm.com>
2022-05-09 18:09:01 +0300

    change browser string

--- sources/official/mirror.sh
+++ sources/official/mirror.sh
@@ -4,8 +4,7 @@ cd $(dirname $0)
 
 if [[ $(jq -r .source.url meta.json) == http* ]]
 then
-  CURLOPTS='-L -c /tmp/cookies -A eps/1.2'
-  curl $CURLOPTS -o official.html $(jq -r .source.url meta.json)
+  curl -L -c /tmp/cookies -A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9' -o official.html $(jq -r .source.url meta.json)
 fi
 
 cd ~-
