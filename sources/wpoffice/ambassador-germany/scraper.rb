a162c10a6840f379d0e9b49985df95bbe6ba0b91
Tony Bowden <tony@tmtm.com>
2022-07-11 08:31:06 +0300

    compare Ambassadors to Germany

---
+++ sources/wpoffice/ambassador-germany/scraper.rb
@@ -0,0 +1,27 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator WikidataIdsDecorator::Links
+
+  def holder_entries
+    noko.xpath("//h3[.//span[contains(.,'Ukraine')]]//following-sibling::ul[1]//li")
+  end
+
+  class Officeholder < OfficeholderNonTableBase
+    def name_node
+      noko.css('a').last
+    end
+
+    def raw_combo_date
+      noko.text.split(':').first
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
